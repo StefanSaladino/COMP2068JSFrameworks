8bf131f045a3e69ccd919b461c234d61ae55f8f8
StefanSaladino <200551988@student.georgianc.on.ca>
2024-05-28 13:44:56 -0400

    added replay function
    User can select whether or not they'd like to start a new game

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -1,77 +1,111 @@
-var prompt = require('prompt');
+var prompt = require("prompt");
+
+prompt.start();
 
 // Define the schema for the input
 const schema = {
-    properties: {
-      userSelection: {
-        description: 'Choose ROCK, PAPER, or SCISSORS',
-        pattern: /^(rock|paper|scissors)$/i,
-        message: 'Selection must be ROCK, PAPER, or SCISSORS',
-        required: true,
-        before: function(value) {
-          return value.trim().toLowerCase();
-        }
-      }
-    }
-  };
-    
+  properties: {
+    userSelection: {
+      description: "Choose ROCK, PAPER, or SCISSORS",
+      pattern: /^(rock|paper|scissors)$/i,
+      message: "Selection must be ROCK, PAPER, or SCISSORS",
+      required: true,
+      before: function (value) {
+        return value.trim().toLowerCase();
+      },
+    },
+  },
+};
+
+const replaySchema = {
+  properties: {
+    playAgain: {
+      description: "Play Again",
+      pattern: /^(yes|no)$/i,
+      message: "Selection must be yes or no",
+      required: true,
+      before: function (value) {
+        return value.trim().toLowerCase();
+      },
+    },
+  },
+};
+
 // Prompt the user for input
-function userInput(){
-prompt.get(schema, (err, result) => {
+function userInput() {
+
+  prompt.get(schema, (err, result) => {
     if (err) {
       console.error(err);
       return;
     }
-  
+
     // Store the processed input in userSelection
     const userSelection = result.userSelection;
-  
+
     // Output the processed input
     console.log(`You selected: ${userSelection}`);
 
-    var prompt = require('prompt');
+    computerChoice(userSelection);
+    // Define the schema for the input
+  });
+}
 
-  // Start the prompt
-  prompt.start();
-  computerChoice(userSelection);
-// Define the schema for the input
-})
-};
-    
 //computer turn
-function computerChoice(userSelection){
-const randomFloat = Math.random();
-let compThrow;
+function computerChoice(userSelection) {
+  const randomFloat = Math.random();
+  let compThrow;
 
-if (randomFloat< 0.35){
-    compThrow = 'paper'
-}
-
-else if (randomFloat> 0.34 && randomFloat< 0.68){
-    compThrow = 'rock'
-}
-
-else{
-    compThrow = 'scissors'
-}
-
-console.log('Computer selected: ' + compThrow);
-
-determineWinner(userSelection, compThrow);
-}
-
-function determineWinner(userSelection, compThrow){
-  if (userSelection == 'rock' && compThrow == 'scissors' || userSelection == 'paper' && compThrow == 'rock' ||
-  userSelection == 'scissors' && compThrow == 'paper')
-  {
-    console.log('YOU WIN');
+  if (randomFloat < 0.35) {
+    compThrow = "paper";
+  } else if (randomFloat > 0.34 && randomFloat < 0.68) {
+    compThrow = "rock";
+  } else {
+    compThrow = "scissors";
   }
-  else if (userSelection == compThrow){
-    console.log('TIE GAME');
-  }
-  else {
-    console.log('YOU LOSE');
-  }
+
+  console.log("Computer selected: " + compThrow);
+
+  determineWinner(userSelection, compThrow);
 }
+
+function determineWinner(userSelection, compThrow) {
+  if (
+    (userSelection == "rock" && compThrow == "scissors") ||
+    (userSelection == "paper" && compThrow == "rock") ||
+    (userSelection == "scissors" && compThrow == "paper")
+  ) {
+    console.log("YOU WIN");
+  } else if (userSelection == compThrow) {
+    console.log("TIE GAME");
+  } else {
+    console.log("YOU LOSE");
+  }
+  replay();
+}
+
+//prompting user to play again or not
+function replay() {
+  //second prompt
+  prompt.get(replaySchema, (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const playAgainSelection = result.playAgain;
+
+    //returned to main prompt if user wants to play again
+    if(playAgainSelection == 'yes'){
+      console.log('New game loading\n')
+      userInput();
+    }
+    //game exits if user doesnt want to play again
+    else if (playAgainSelection == 'no'){
+      console.log('Goodbye');
+      process.exit();
+    }
+
+}
+)};
 
 userInput();
