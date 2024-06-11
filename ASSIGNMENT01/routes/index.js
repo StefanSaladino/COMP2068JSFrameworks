6f7c815fbf820c1cd8b3437bebf3bd7f5e6b52bc
StefanSaladino <200551988@student.georgianc.on.ca>
2024-06-11 14:42:39 -0400

    added global footer/index page info

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My portfolio' });
+  res.render('index', { title: `Stefan Saladino's portfolio` });
 });
 
 module.exports = router;
